5bcd9b831ea5affe252fcffd8c56c0d8dee3e70a
tmdelrosario <thristandelrosario@gmail.com>
2021-09-10 17:10:18 +0800

    added service controller class

---
+++ Coding/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Coding.Models;
+using System.Net.Http;
+
+namespace Coding.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            IEnumerable<UsersModel> userlist = null;
+
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44329/api/");
+
+            var responseTask = hc.GetAsync("values");
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var displayresults = result.Content.ReadAsAsync<IList<UsersModel>>();
+                displayresults.Wait();
+
+                userlist = displayresults.Result;
+            }
+
+            return View(userlist);
+        }
+
+        public ActionResult Getuser(int id)
+        {
+
+            IEnumerable<UsersModel> userlist = null;
+
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44329/api/");
+
+            var responseTask = hc.GetAsync("values/" + id);
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var displayresults = result.Content.ReadAsAsync<IList<UsersModel>>();
+                displayresults.Wait();
+
+                userlist = displayresults.Result;
+            }
+
+            return Json(userlist, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
